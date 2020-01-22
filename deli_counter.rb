49b601534b0f0d37c580242a310638de742a7c4f
Micah Banschick <micha.ban@gmail.com>
2020-01-22 19:00:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(arr)
   if arr.length > 0 
     n = []
-    arr.each {|name| n << "#{arr.index(name) + 1}  #{name}"}
+    arr.each {|name| n << "#{arr.index(name) + 1}.  #{name}"}
     puts "The line is currently: #{n.join(" ")}"
   else 
     puts "The line is currently empty."
